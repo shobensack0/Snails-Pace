bd63bc1fd3fd6385c534dfb60141b577cb0e8296
shobensack0 <shobensack0@gmail.com>
2019-11-03 15:27:09 -0500

    add camera

---
+++ Assets/scripts/utility/BasicCamera.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class BasicCamera : MonoBehaviour
+{
+    public GameObject target;
+    public Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target)
+        {
+            transform.position = new Vector3(
+                target.transform.position.x + offset.x, 
+                target.transform.position.y + offset.y, 
+                target.transform.position.z + offset.z);
+        }
+    }
+}
