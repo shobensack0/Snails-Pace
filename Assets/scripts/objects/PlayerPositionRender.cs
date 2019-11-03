738737576fdac7b54db717c69d80fd3a3c32edc3
shobensack0 <shobensack0@gmail.com>
2019-11-03 11:56:38 -0500

    some light file clean up, tree sorting order

---
+++ Assets/scripts/objects/PlayerPositionRender.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class PlayerPositionRender : MonoBehaviour
+{
+    private GameObject player;
+    private BoxCollider2D player_BoxCollider;
+    private SpriteRenderer spriteRenderer;
+    private BoxCollider2D collider;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+
+        if (player != null)
+        {
+            player.TryGetComponent<BoxCollider2D>(out player_BoxCollider);
+        }
+
+        TryGetComponent<SpriteRenderer>(out spriteRenderer);
+        TryGetComponent<BoxCollider2D>(out collider);
+    }
+
+    void Update()
+    {
+        if (player_BoxCollider.transform.position.y <= collider.bounds.center.y)
+        {
+            // render object behind player
+            this.spriteRenderer.sortingOrder = 0;
+        } else
+        {
+            // render object in front of player
+            this.spriteRenderer.sortingOrder = 10;
+        }
+    }
+}
